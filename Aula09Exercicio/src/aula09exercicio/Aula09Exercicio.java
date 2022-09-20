ee4f058623fd8381c3003509c7761c84b6d89d5b
Felipe Alexandre Duarte Araújo Ramos <felipeadar108@gmail.com>
2022-09-20 14:27:41 -0300

    Exercicio 9.
    Exercício envolvendo classes e subclasses.

---
+++ Aula09Exercicio/src/aula09exercicio/Aula09Exercicio.java
@@ -0,0 +1,21 @@
+package aula09exercicio;
+public class Aula09Exercicio {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+        
+        p[0] = new Pessoa("Pedro", 22,  "M");
+        p[1] = new Pessoa("Felipe", 19, "M");
+        
+        l[0] = new Livro("Aprendendo Java", "José da Silva", 300, p[0]);
+        l[1] = new Livro("POO para Iniciantes", "Pedro Paulo", 500, p[1]);
+        l[2] = new Livro("Java avançado", "Maria Candido", 800, p[0]);
+        
+        l[0].abrir();
+        l[0].folhear(100);
+        System.out.println(l[0].detalhes());
+    }
+    
+}
